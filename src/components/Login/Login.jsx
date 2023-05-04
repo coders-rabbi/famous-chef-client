7da1d2c3890e1b43d0100c9e0c43fbc526fdd131
coders rabbi <pcrabbi2020@gmail.com>
2023-05-04 12:05:15 +0600

    registration page design complete!

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
                 <div className="hero-content flex-col">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold text text-center">Please Login!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p className="py-6 text-center">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form onSubmit={handleLogin} className="card-body">
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
+
+    const handleRegistration = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,email, password);
+    }
+
     return (
-        <div>
-            <h1>Registration page comming soon!</h1>
+        <div className='container mx-auto mt-20 min-h-screen'>
+            <div className="w-3/6 mx-auto bg-header-bg py-14 px-7 rounded-2xl">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold text text-center">Please Registration!</h1>
+                        <p className="py-6 text-center">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleRegistration} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                        <p className='text-center mb-5 text-black'>
+                            <Link to="/login">Already have an account? Please Login</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
