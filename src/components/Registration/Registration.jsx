32abc6bc624a53a60b4df031e117f096767e8739
coders rabbi <pcrabbi2020@gmail.com>
2023-05-04 12:37:18 +0600

    react reatting added successfully

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -27,19 +27,25 @@ const Registration = () => {
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="Photo" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                                <input type="email" name='email' placeholder="Email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <input type="password" name='password' placeholder="Password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
--- src/components/SingleRecipe/SingleRecipe.jsx
+++ src/components/SingleRecipe/SingleRecipe.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import Rating from 'react-rating';
+import { FaRegStar,FaStar } from "react-icons/fa";
 
-const SingleRecipe = ({singleRecipe}) => {
-    const {recipies_name,ingradients,cooking_methods,rating} = singleRecipe;
+const SingleRecipe = ({ singleRecipe }) => {
+    const { recipies_name, ingradients, cooking_methods, rating } = singleRecipe;
     return (
         <div className=' p-3 rounded-md bg-header-bg shadow-emerald-50'>
             <h1 className='mt-3'><span className='font-bold'>Recipe Name:</span> {recipies_name}</h1>
             <p className='mt-3'><span className='font-bold leading-6'>Ingradients: <br /> </span> <span className='leading-6 text-neutral-500'>{ingradients}</span></p>
             <p className='mt-3'><span className='font-bold'>Cooking_Methods: <br /></span> <span className='text-neutral-500'>{cooking_methods}</span></p>
-            <p className='mt-3'>{rating}</p>
+            <div className='flex items-center gap-4'>
+                <Rating
+
+                    placeholderRating={rating}
+                    readonly
+                    emptySymbol={
+                        <FaRegStar></FaRegStar>
+                    }
+                    placeholderSymbol={
+                        <FaStar></FaStar>
+                    }
+                    fullSymbol={
+                        <FaStar></FaStar>
+                    }
+                />
+                <p className='mt-3 text-2xl'>{rating}</p>
+            </div>
         </div>
     );
 };
